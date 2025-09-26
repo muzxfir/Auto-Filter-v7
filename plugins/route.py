a3f91ce3fd2202b95e315bcb7e85a5539a968cc8
𝗧𝗚𝗘𝗕𝗢𝗧𝗭 <97307696+TGEBOTZ@users.noreply.github.com>
2025-09-26 05:43:11 +0400

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -6,12 +6,12 @@ import secrets
 import time
 import mimetypes
 from aiohttp.http_exceptions import BadStatusLine
-from dreamxbotz.Bot import multi_clients, work_loads, dreamxbotz
-from dreamxbotz.server.exceptions import FIleNotFound, InvalidHash
-from dreamxbotz.zzint import StartTime, __version__
-from dreamxbotz.util.custom_dl import ByteStreamer
-from dreamxbotz.util.time_format import get_readable_time
-from dreamxbotz.util.render_template import render_page
+from tgebotz.Bot import multi_clients, work_loads, tgebotz
+from tgebotz.server.exceptions import FIleNotFound, InvalidHash
+from tgebotz.zzint import StartTime, __version__
+from tgebotz.util.custom_dl import ByteStreamer
+from tgebotz.util.time_format import get_readable_time
+from tgebotz.util.render_template import render_page
 from info import *
 
 
@@ -19,7 +19,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("dreamxbotz")
+    return web.json_response("tgebotz")
 
 @routes.get(r"/watch/{path:\S+}", allow_head=True)
 async def stream_handler(request: web.Request):
